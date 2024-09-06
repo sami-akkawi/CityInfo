48b3d28d8d5ceff427ed70f058b66ef606c8f8ea
Sami Akkawi <sami@akkawi.ch>
2024-09-06 11:19:05 +0200

    Introduce Child Resources

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -6,25 +6,58 @@ public class CitiesDataStore
 {
     public List<CityDto> Cities { get; set; } =
     [
-        new CityDto()
+        new()
         {
             Id = 1,
             Name = "New York City",
             Description = "The one with that big park.",
+            PointsOfInterest = new List<PointOfInterestDto>()
+            {
+                new() {
+                    Id = 1,
+                    Name = "Central Park",
+                    Description = "The most visited urban park in the United States." },
+                new() {
+                    Id = 2,
+                    Name = "Empire State Building",
+                    Description = "A 102-story skyscraper located in Midtown Manhattan." },
+            }
         },
 
-        new CityDto()
+        new()
         {
             Id = 2,
             Name = "Antwerp",
             Description = "The one with the cathedral that was never really finished.",
+            PointsOfInterest = new List<PointOfInterestDto>()
+            {
+                new() {
+                    Id = 3,
+                    Name = "Cathedral of Our Lady",
+                    Description = "A Gothic style cathedral, conceived by architects Jan and Pieter Appelmans." },
+                new() {
+                    Id = 4,
+                    Name = "Antwerp Central Station",
+                    Description = "The the finest example of railway architecture in Belgium." },
+            }
         },
 
-        new CityDto()
+        new()
         {
             Id = 3,
             Name = "Paris",
             Description = "The one with that big tower.",
+            PointsOfInterest = new List<PointOfInterestDto>()
+            {
+                new() {
+                    Id = 5,
+                    Name = "Eiffel Tower",
+                    Description = "A wrought iron lattice tower on the Champ de Mars, named after engineer Gustave Eiffel." },
+                new() {
+                    Id = 6,
+                    Name = "The Louvre",
+                    Description = "The world's largest museum." },
+            }
         }
     ];
 
---
+++ CityInfo.API/Controller/PointsOfInterestController.cs
@@ -0,0 +1,39 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controller;
+
+[Route("api/cities/{cityId}/pointsofinterest")]
+[ApiController]
+public class PointsOfInterestController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+    {
+        CityDto? city = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == cityId);
+        if (city == null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(city.PointsOfInterest);
+    }
+
+    [HttpGet("{pointOfInterestId}")]
+    public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId)
+    {
+        CityDto? city = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == cityId);
+        if (city == null)
+        {
+            return NotFound();
+        }
+        
+        PointOfInterestDto? pointOfInterest = city.PointsOfInterest.FirstOrDefault(point => point.Id == pointOfInterestId);
+        if (pointOfInterest == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(pointOfInterest);
+    }
+}
