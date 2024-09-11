299ac878fb788d17f65e8de5335ad5d69ff6b70c
Sami Akkawi <sami@akkawi.ch>
2024-09-11 07:00:57 +0200

    Add validations to creation endpoint

--- CityInfo.API/Models/PointOfInterestForCreationDto.cs
+++ CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -1,7 +1,13 @@
-﻿namespace CityInfo.API.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models;
 
 public class PointOfInterestForCreationDto
 {
+    [Required(ErrorMessage = "field_is_required")]
+    [MaxLength(50)]
     public string Name { get; set; } = string.Empty;
+    
+    [MaxLength(200)]
     public string? Description { get; set; }
 }
