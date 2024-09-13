0214d98fabd1818f1f2a010388b27fa4a48a0f6e
Sami Akkawi <sami@akkawi.ch>
2024-09-13 07:22:52 +0200

    Use Support for JsonPatchDocument

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers(options => options.ReturnHttpNotAcceptable = true)
+    .AddNewtonsoftJson()
     .AddXmlDataContractSerializerFormatters();
 builder.Services.AddProblemDetails(
     options => 
