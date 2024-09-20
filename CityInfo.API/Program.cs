5d2577dcbf5dc628dc44848a637810cbe0a53ec9
Sami Akkawi <sami@akkawi.ch>
2024-09-20 07:35:06 +0200

    Log to file using Serilog

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,6 +1,14 @@
 using Microsoft.AspNetCore.StaticFiles;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog();
 
 // Add services to the container.
 builder.Services.AddControllers(options => options.ReturnHttpNotAcceptable = true)
