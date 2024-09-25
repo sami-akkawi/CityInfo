83be7763ab720c29b423faf438bdd69d4ddf4264
Sami Akkawi <sami@akkawi.ch>
2024-09-25 07:13:14 +0200

    Define Entities

---
+++ CityInfo.API/Entities/City.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities;
+
+public class City(string name)
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; } = name;
+    
+    [MaxLength(200)]
+    public string? Description { get; set; }
+
+    public ICollection<PointOfInterest> PointOfInterests { get; set; } = new List<PointOfInterest>();
+}
---
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities;
+
+public class PointOfInterest(string name)
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    [ForeignKey("CityId")]
+    public City? City { get; set; }
+    
+    public int CityId { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; } = name;
+}
